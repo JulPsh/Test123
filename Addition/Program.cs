ef90be3d774cca52fcd8dc87236874a7379777f1
JulPsh <6523+JulPsh@users.noreply.github.com>
2019-11-30 17:46:51 +0300

    Test commit

---
+++ Addition/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Addition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+            int y = 10;
+            int z = x + y;
+            Console.WriteLine($"Первое число:{x}");
+            Console.WriteLine($"Второе число:{y}");
+            Console.WriteLine($"Сумма:{z}");
+        }
+    }
+}
